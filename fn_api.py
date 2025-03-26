4cad1e50b0bac5e0ccfdfda6706984e4773b00c7
lkxoayh <65092891+lkxoayh@users.noreply.github.com>
2025-03-26 22:31:48 +0100

    Update fn_api.py

--- fn_api.py
+++ fn_api.py
@@ -735,3 +735,6 @@ def create_task():
 super_token = GetTokenPC()
 no_eg1_token = GetTokenPCNotEG1()
 public_token = GetTokenPublic()
+user_agent = 'Fortnite/++Fortnite+Release-34.20-CL-40965447 Windows/10.0.19045.1.256.64bit'
+version, digits = user_agent[user_agent.index("Release-") + len("Release-") : user_agent.index("-CL-")], user_agent[user_agent.index("CL-") + len("CL-") : user_agent.index(" W")]
+disco_token = access_discovery(branch=f"++Fortnite+Release-{version}")
